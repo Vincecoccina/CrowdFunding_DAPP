bd3611611fb23a085eac98f49c7ab94cbdd5fc09
Vince <v.coccina@orange.fr>
2023-03-24 17:25:27 +0100

    responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Navbar.module.css";
 import { Link, useNavigate } from "react-router-dom";
-import { logo } from "../assets/index";
+import Sidebar from "./Sidebar";
+import { logo, menu } from "../assets/index";
 import Button from "./shared/Button";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   const navigate = useNavigate();
   const adress = "0xcB1441CB818034DF5E235b0bc71638B536c5F9e3";
 
@@ -31,10 +33,49 @@ const Navbar = () => {
           </Button>
           <Link to="/profile">
             <div className={classes.containerAvatar}>
-              <img src={logo} alt="" />
+              <img src={logo} alt="logo" />
             </div>
           </Link>
         </div>
+
+        {/* Mobile screen */}
+        <div className={classes.containerMobile}>
+          <div className={classes.iconMobile}>
+            <div
+              className={classes.containerBurger}
+              onClick={() => setToggleMenu((prev) => !prev)}
+            >
+              <img src={menu} alt="" />
+            </div>
+            <Link to="/profile">
+              <div className={classes.containerAvatar}>
+                <img src={logo} alt="logo" />
+              </div>
+            </Link>
+          </div>
+        </div>
+
+        {toggleMenu && (
+          <div
+            className={`${classes.sideBarMobile} ${
+              toggleMenu ? classes.translate : ""
+            }`}
+          >
+            <Sidebar />
+            <div className={classes.btnMobile}>
+              <Button
+                btnType="button"
+                handleClick={() => {
+                  if (adress) {
+                    navigate("/addProject");
+                  }
+                }}
+              >
+                {adress ? "Create Campaign" : "Connect Wallet"}
+              </Button>
+            </div>
+          </div>
+        )}
       </nav>
     </header>
   );
